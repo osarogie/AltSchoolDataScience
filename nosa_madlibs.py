e8fd0c331291580f55b65036894f704f6554b052
Emmanuel Nosakhare <nonreactiv@gmail.com>
2023-03-19 17:25:01 +0100

    first semester project

---
+++ nosa_madlibs.py
@@ -0,0 +1,21 @@
+print("Pastor: Testimony Time!?")
+print("Congregation: Blessing Time!!")
+print("Pastor: Let's take the first testimony\n")
+
+print("Praaaaaise the Lord!!")
+print("Congregation: Hallelujah!!")
+
+name = input("My name is: ")
+age = int(input("My age: "))
+occupation = input("I work as a/an: ")
+request = input("I've been praying to the Lord for: ")
+print("Pastor: How long have you been praying for this?")
+period = input("I've been praying for this for: ")
+testimony = input("And what I am most grateful for is: ")
+seed = input("To show my gratitude, I would like to give: ")
+
+print(
+    f"\nMy name is {name} and I am {age} years old. I work as a {occupation}. I've been praying to the Lord for {request} for {period}. And what I am most grateful for is {testimony}. To show my gratitude, I would like to give to the amount of {seed}."
+)
+print("Praaaaaise the Lord!!")
+print("Congregation: Hallelujah!!\n")
